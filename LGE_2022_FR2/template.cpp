8a837ec5e15eaa653ed8e38e402aeffc2a255b15
haipng <haiphamece@gmail.com>
2024-01-10 21:48:43 +0700

    template update

--- LGE_2022_FR2/template.cpp
+++ LGE_2022_FR2/template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 #define pb push_back
 #define FOR(_i, _a, _b) for (int _i = (_a); _i < (_b); _i++)
+#define FORE(it,x) for(__typeof(x.begin()) it = x.begin(); it != x.end(); ++it)
 
 typedef long long              ll;
 typedef pair<int,int>         pii;
@@ -11,14 +12,18 @@ typedef vector<int>            vi;
 typedef vector<vector<int>>   vvi;
 typedef vector<ll>            vll;
 
+const ll MOD   = ll(1e9)+7;
+const ll LLINF = ll(1e18);
+const int INF  = int(1e9);
+
 void solve();
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 
-    int t;
+    int t=1;
     cin >> t;
     while(t--) {
         solve();
@@ -27,10 +32,7 @@ int main() {
     return 0;
 }
 
-const ll MOD   = ll(1e9)+7;
-const ll LLINF = ll(1e18);
-const int INF  = int(1e9);
-
+/***** START *****/  
 
 void solve() {
     cout << "To " << INF << " and beyond\n";
